50803fc446c4dcfb8b4655f4ac9cc397b6f1e087
shreyasgs10 <68457999+shreyasgs10@users.noreply.github.com>
2021-11-24 10:16:17 +0530

    Add files via upload

---
+++ BLOG/assets/js/script.js
@@ -0,0 +1,63 @@
+// repeated variables
+var $window = $(window);
+var $root = $("html, body");
+
+$(document).ready(function () {
+  "use strict";
+  typedJS();
+});
+
+function menuToggler() {
+  "use strict";
+
+}
+/*-------------------------
+        TYPED JS 
+    www.mattboldt.com
+-------------------------*/
+function typedJS() {
+  "use strict";
+
+  var options = {
+    strings: [
+      'Web Master',
+      'Web Designer',
+      'Web Developer',
+      'Front End Web Developer',
+      'Back End Web Developer',
+      'Software Developer'
+    ],
+    typeSpeed: 30,
+    loop: true,
+    backDelay: 3000,
+    backSpeed: 30,
+    smartBackspace: true,
+    showCursor: true,
+    cursorChar: '#',
+    shuffle: true,
+    autoInsertCss: true,
+    fadeOut: true,
+    fadeOutClass: 'typed-fade-out',
+    fadeOutDelay: 500
+  };
+  var typed = new Typed(".element", options);
+}
+
+
+/*------------------------
+      Scroll to Top
+--------------------------*/
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 100) {
+    $('.back-to-top').fadeIn('slow');
+  } else {
+    $('.back-to-top').fadeOut('slow');
+  }
+});
+
+$('.back-to-top').click(function () {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 1500, 'easeInOutExpo');
+  return false;
+});
